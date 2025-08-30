ce25c352ab1fb515f0ace5c3af64189e73ccb8cf
Safiullah Maaz <safiullahmaaz59@gmail.com>
2025-08-30 12:15:17 +0500

    added promise in polling

--- src/utils/services.js
+++ src/utils/services.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_KEY = '';
 const BASE_URL = 'https://techhk.aoscdn.com';
 const MAX_LIMIT = 20;
+const POLLING_INTERVAL_MS = 2000;
 
 export const enhancedImageApi = async (file) => {
     try {        
@@ -11,7 +12,7 @@ export const enhancedImageApi = async (file) => {
         return enhancedImageData;
     } catch (err) {
         console.log("Error in image enhancing : ", err.message)
-        return err
+        throw err
     }
 }
 
@@ -27,7 +28,7 @@ const uploadImage = async (file) => {
     if(!data?.data?.task_id) {
         throw new Error("Failed to upload image! Task id not found")
     }
-    return data?.data?.task_id;
+    return data.data.task_id;
 }
 
 const fetchEnhancedImage = async (taskId) => {
@@ -41,7 +42,7 @@ const fetchEnhancedImage = async (taskId) => {
         throw new Error("Failed to fetch enhanced image.");
     }
 
-    return data?.data?.image;
+    return data.data.image;
 }
 
 const pollingForEnhancedImage = async (taskId, tries = 0) => {
@@ -52,6 +53,7 @@ const pollingForEnhancedImage = async (taskId, tries = 0) => {
             throw new Error("Max limit reached to fetch images");
         }
 
+        await new Promise((resolve) => setTimeout(resolve, POLLING_INTERVAL_MS))
         return pollingForEnhancedImage(taskId, tries + 1)
     }
 
